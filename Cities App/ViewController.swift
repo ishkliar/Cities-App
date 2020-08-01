b6d2867ed5134ef89df58def0ae15710a130afeb
Iaroslav Shkliar (PGS Software) <iashkliar@pgs-soft.com>
2020-08-01 13:25:05 +0200

    Initial Commit

---
+++ Cities App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cities App
+//
+//  Created by Iaroslav Shkliar on 01/08/2020.
+//  Copyright © 2020 Iaroslav Shkliar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
